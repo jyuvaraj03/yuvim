3686f2c0175fb702719cb76b330f05219b8f6646
Yuvaraj J <yuvaraj.jayavel@chronus.com>
2025-02-03 14:20:25 +0530

    Remove treesitter indentation

--- lua/plugins/others.lua
+++ lua/plugins/others.lua
@@ -51,7 +51,6 @@ return {
           enable = true,
           additional_vim_regex_highlighting = false,
         },
-        indent = { enable = true },
         textobjects = {
           swap = {
             enable = true,
